7e0af9fb6d82fb4264968eef0ef4409838850b1b
zvrkhanuma <almazalovic@gmail.com>
2021-03-10 23:15:22 +0100

    Added eight index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+
+    var sum = numOne + numTwo;
+    return sum;
+
+   
+
+    // Only change code above this line
+
+  
+    
+}
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+
+module.exports = main;
