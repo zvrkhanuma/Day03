e3c06fea598a71fda46a4dc7181ef4953833cfbd
zvrkhanuma <almazalovic@gmail.com>
2021-03-10 23:28:55 +0100

    Added tenth index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function main (numOne, numTwo) {
+    // Only change code below this line
+
+    var product = numOne * numTwo;
+    return product;
+
+   
+
+    // Only change code above this line
+
+  
+    
+}
+
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+
+module.exports = main;
