e1af634ee30b481ead3fc4f63218bab236969f2d
zvrkhanuma <almazalovic@gmail.com>
2021-03-10 22:49:57 +0100

    Added sevnth index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+
+    }
+    
+}
+console.log(main());
+
+
+module.exports = main;
