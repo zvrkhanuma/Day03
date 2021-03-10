7fb2282cb5317dd0c3bfecd56179c40b96490417
zvrkhanuma <almazalovic@gmail.com>
2021-03-10 23:20:52 +0100

    Added ninth index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+
+    var difference = numOne - numTwo;
+    return difference;
+
+   
+
+    // Only change code above this line
+
+  
+    
+}
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
+
+
+module.exports = main;
