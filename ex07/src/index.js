4f07b1d8d69b8e481c63b1f8778a05de628bdf65
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 10:06:33 +0100

    Modified seventh index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,8 +1,9 @@
 function main() {
+    // Variable declarations
     var studlyCapVar;
     var properCamelCase;
     var titleCaseOver;
-
+    // Variable assignments
     studlyCapVar = 10;
     properCamelCase = "A String";
     titleCaseOver = 9000;
