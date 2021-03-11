6226eba94cb8056d0dac2a276fe59e0d917511f1
zvrkhanuma <almazalovic@gmail.com>
2021-03-11 10:02:25 +0100

    Modified  sixth index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,13 +1,14 @@
 function main() {
+   // Only change code below this line
     var x = 7;
 
     var y = 12;
 
-    var z = "My name is ";
-
+    var z = "My name is";
+   // Only change code above this line
     x = x + 2;
     y = y + 4;
-    z = z + "John!";
+    z = z + " John!";
 
     return [x, y, z];
     
